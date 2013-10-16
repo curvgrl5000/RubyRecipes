50c990e521260506024d0bdffa8b86b0532e4a64
anna mendoza <anna@curvecommunications.biz>
2013-10-16 11:56:58 -0700

    updated repo with correct file

--- BettyCrockerMuffins/Recipe4_BettyMuffins.rb
+++ BettyCrockerMuffins/Recipe4_BettyMuffins.rb
@@ -31,7 +31,7 @@ class Recipe
     puts "Frosting"
     puts "-" * 30
     @decorations.each_with_index do |decorate, i|
-      puts "#{i+1}. #{step}"
+      puts "#{i+1}. #{decorate}"
     end
     
     ""
@@ -52,17 +52,17 @@ puts "Your choice was: ", choice, "\n"
 puts "Okay, here's your #{yum} recipe. Enjoy!\n" if yum == choice      
 
 # The Recipe...
-muffins = Recipe.new("Betty's Hard-core #{choice} Muffins", " 2 cups Original Bisquick® mix", " 1 cup water", " 1/3 cup veg. oil", "4 1/2 teaspoons egg replacer", "3/4 cup fresh or frozen (thawede and drained ) blueberries",
+muffins = Recipe.new("Betty's Hard-core #{choice} Muffins", " 2 cups Original Bisquick mix", " 1 cup water", "1/3 cup veg. oil", "4 1/2 teaspoons egg replacer", "3/4 cup fresh or frozen (thawed and drained ) blueberries",
 "1 cup of Crisco", "2 cups confectioner sugar", "1/4 cup of water", "1 tsp. lemon extract")      
 puts muffins.steps #=> []
-muffins.steps[0] = "Heat oven to 400ºF. Place paper baking cup in each of 12 regular-size muffin cups, or grease bottoms only of muffin cups."
+muffins.steps[0] = "Heat oven to 400F. Place paper baking cup in each of 12 regular-size muffin cups, or grease bottoms only of muffin cups."
 muffins.steps[1] = "Stir all ingredients except blueberries just until moistened. Gently stir in blueberries. Divide batter evenly among cups."
 muffins.steps[2] = "Mix just until dry ingredients are moistened. Overmixing causes tunnels and a tougher texture in the muffins."  
 muffins.steps[3] = "Bake 15 to 25 minutes or until golden brown." 
-muffins.deocorations[0] = "Mix Crisco, confectioner sugar, 1/4 cupe of water and lemon extract together"
-muffins.deocorations[1] = "Whip to a creamy texture and get ready to decorate."
-muffins.deocoration[2] = "Take one hefty spoonsize of frosting and place over muffin, spreading evenly with
+muffins.decorations[0] = "Mix Crisco, confectioner sugar, 1/4 cupe of water and lemon extract together"
+muffins.decorations[1] = "Whip to a creamy texture and get ready to decorate."
+muffins.decorations[2] = "Take one hefty spoonsize of frosting and place over muffin, spreading evenly with
 the back of the spoon. Don't lick that spoon, ladies!"            
-muffins.deocoration[3] = "Sprinkle and serve!"
+muffins.decorations[3] = "Sprinkle and serve!"
 puts muffins
 
